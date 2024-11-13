8191dbd7504ca99c41e6f876934b71029f78daa2
Nicolas <blanc-nicolas@outlook.com>
2024-11-13 16:30:21 +0100

    back

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
+const fetch = require('node-fetch'); // si besoin d'utiliser fetch pour du HTTP
 
 const OWM_API_KEY = process.env.OWM_API_KEY
 
-const fetch = require('node-fetch'); // si besoin d'utiliser fetch pour du HTTP
 
 router.get('/movies', (req,res) => {
     fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${OWM_API_KEY}`) 
