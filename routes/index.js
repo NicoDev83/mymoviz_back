cb0f6b88399875bb38c9476c085f3d8dab82b992
Nicolas <blanc-nicolas@outlook.com>
2024-11-13 16:34:56 +0100

    back

--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,12 @@ const fetch = require('node-fetch'); // si besoin d'utiliser fetch pour du HTTP
 
 const OWM_API_KEY = process.env.OWM_API_KEY
 
-
 router.get('/movies', (req,res) => {
     fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${OWM_API_KEY}`) 
     .then(response => response.json())
-    .then(apiData => {
-        console.log(apiData)
-        res.json({movies: apiData.results})
+    .then(data => {
+        console.log(data)
+        res.json({movies: data.results})
     })
 })
 
